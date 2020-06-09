99ab8f1531a1103d24532db142ffef511523024f
ArtDark <balusoff@gmail.com>
2020-06-09 09:03:35 +0300

    Revert "Change some vars to const"
    This reverts commit 2054495d21aa6ccfa66dd7f796c760fbcdb518c0.

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ func main() {
 	purchase := 3333_33
 	percent := 1
 	limit := 100
-	const toDollars = 100
-	const oneHundredPercent = 100
+	toDollars := 100
+	oneHundredPercent := 100
 
 	bonus := (purchase * percent / oneHundredPercent) / toDollars // вместо нуля ваша формула
 	if bonus > limit {
